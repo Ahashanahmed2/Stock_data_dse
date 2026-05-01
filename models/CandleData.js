f13d0551758112ab37b6f29be376ae96cdb2aca4
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-05-01 19:54:15 +0800

    Update CandleData.js

--- models/CandleData.js
+++ models/CandleData.js
@@ -1,5 +1,5 @@
 //#অটিজিনাল
-const mongoose = require('mongoose');
+/* mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CandleDataSchema = new Schema({
@@ -29,4 +29,48 @@ const CandleDataSchema = new Schema({
 // ✅ যুক্ত করো এই লাইন
 CandleDataSchema.index({ symbol: 1, date: 1 }, { unique: true });
 
+module.exports = mongoose.model('CandleData', CandleDataSchema); */
+
+//উপরের টা অরিজিনাল
+
+
+
+
+//#অটিজিনাল
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CandleDataSchema = new Schema({
+  symbol: {
+    type: String,
+    required: true,
+    index: true
+  },
+  date: {
+    type: String,
+    required: true,
+    index: true
+  },
+  open: { type: Number, default: null },
+  close: { type: Number, default: null },
+  high: { type: Number, default: null },
+  low: { type: Number, default: null },
+  volume: { type: Number, default: null },
+  value: { type: Number, default: null },
+  trades: { type: Number, default: null },
+  change: { type: Number, default: null },
+  marketCap: { type: Number, default: null },
+  freeFloatMarketCap: { type: Number, default: null },  // ✅ NEW
+  sector: { type: String, default: null }
+},
+            { upsert: true }                         );
+
+// ✅ যুক্ত করো এই লাইন
+CandleDataSchema.index({ symbol: 1, date: 1 }, { unique: true });
+
 module.exports = mongoose.model('CandleData', CandleDataSchema);
+
+
+
+
+
